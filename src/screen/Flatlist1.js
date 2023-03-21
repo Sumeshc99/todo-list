8b2db15e37f99d487838d0c86c366c3468462d02
sumeshc99 <sumeshchaure99@gmail.com>
2023-03-21 18:47:06 +0530

    added flatlist

---
+++ src/screen/Flatlist1.js
@@ -0,0 +1,70 @@
+import {Button, FlatList, Modal, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+
+const flatlist1 = () => {
+  const [modal1, setmodal1] = useState(false);
+
+  const users = [
+    {
+      id: 1,
+      name: 'sum',
+    },
+    {
+      id: 2,
+      name: 'dum',
+    },
+    {
+      id: 3,
+      name: 'dhdsbf',
+    },
+    {
+      id: 4,
+      name: 'sdffsd',
+    },
+  ];
+
+  const renderFlatList = item => {
+    return (
+      <Text style={{fontSize: 25}}>
+        {item.id}: {item.name}
+      </Text>
+    );
+  };
+
+  return (
+    <View style={{flex: 1}}>
+      <Modal transparent={true} visible={modal1} animationType='fade'>
+        <View style={styles.centeredview}>
+          <View style={styles.modalView}>
+            <Text style={{fontSize: 30, marginBottom: 10}}>
+              {' '}
+              sdsds sdssd modal
+            </Text>
+            <Button title="close modal" onPress={() => setmodal1(false)}/>
+          </View>
+        </View>
+      </Modal>
+      <Text style={{fontSize: 50}}>Flatlist1</Text>
+      <FlatList data={users} renderItem={({item}) => renderFlatList(item)} />
+      <Button title="open modal" onPress={() => setmodal1(true)} />
+    </View>
+  );
+};
+
+export default flatlist1;
+
+const styles = StyleSheet.create({
+  centeredview: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  modalView: {
+    backgroundColor: 'white',
+    padding: 40,
+    borderRadius: 20,
+    shadowColor: 'black',
+    elevation: 30,
+  },
+});
